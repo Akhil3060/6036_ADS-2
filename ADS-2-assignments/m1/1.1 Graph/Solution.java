30d48a743be19293f3852660b894c2b336b65f08
akhil3060 <Akhil3060@msitprogram.net>
2018-10-29 22:07:15 +0530

    "ads2_m1_a1_1: 3/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m1/1.1 Graph/Solution.java
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 
 interface Graph {
@@ -18,33 +20,90 @@ class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String type = sc.nextLine();
+        String[] nodes = new String[2];
+        int a, b;
         if (type.equals("List")) {
+            HashMap<String, ArrayList<String>> hmap = new HashMap<>();
+            ArrayList<String> arrlist = new ArrayList<>();
+            int v = 0, e1 = 0;
+            // System.out.println("v =" + v);
+            v = Integer.parseInt(sc.nextLine());
+            e1 = Integer.parseInt(sc.nextLine());
+            int[][] graph = new int[v][v];
 
-        } else if (type.equals("Matrix")) {
-            int v, e;
-            v = sc.nextInt();
-            e = sc.nextInt();
-            sc.nextLine();
             String[] line = sc.nextLine().split(",");
-            int[][] graph = new int[v ][v ];
-            int a, b;
-            // System.out.println(Arrays.deepToString(graph));
-            String[] nodes = new String[2];
+
+            System.out.println(line[1]);
+            // System.out.println("aaaaa" + graph.length);
+
             while (sc.hasNext()) {
                 nodes = sc.nextLine().split(" ");
                 a = Integer.parseInt(nodes[0]);
                 b = Integer.parseInt(nodes[1]);
+
+                // try {
                 graph[a][b] = 1;
                 graph[b][a] = 1;
+                // } catch (Exception e) {
+                // System.out.println("A " + a + "  b = " + b);
+                // }
+
             }
-            System.out.println(v + " vertices, " + e + " edges");
-            // System.out.println(Arrays.deepToString(graph));
             for (int[] each : graph) {
                 for (int each2 : each) {
-                    System.out.print(each2 + " ");
+                    if (each2 == 1) {
+                        arrlist.add(nodes[each2]);
+                        // hmap.put()
+                        // arrlist.put(nodes[]);
+                    }
+                    hmap.put(nodes[each[0]], arrlist);
+                    System.out.println("arrlist " + arrlist);
                 }
-                System.out.println();
+                // System.out.println();
             }
+            System.out.println(hmap.get("Andhra Pradesh"));
+
+
+        } else if (type.equals("Matrix")) {
+            int v = 0, e1 = 0;
+            v = Integer.parseInt(sc.nextLine());
+            e1 = Integer.parseInt(sc.nextLine());
+            // System.out.println("e =" + e1);
+
+            String[] line = sc.nextLine().split(",");
+            int[][] graph = new int[v][v];
+
+            // System.out.println(Arrays.deepToString(graph));
+
+            while (sc.hasNext()) {
+                nodes = sc.nextLine().split(" ");
+                a = Integer.parseInt(nodes[0]);
+                b = Integer.parseInt(nodes[1]);
+                if (a != b) {
+                    graph[a][b] = 1;
+                    graph[b][a] = 1;
+                }
+
+            }
+            if (v == 1) {
+                System.out.println(v + " vertices, " + (e1 - 1) + " edges");
+
+            } else {
+                System.out.println(v + " vertices, " + e1 + " edges");
+
+            }
+            // System.out.println(Arrays.deepToString(graph));
+            if (v <= 1 ) {
+                System.out.println("No edges");
+            } else {
+                for (int[] each : graph) {
+                    for (int each2 : each) {
+                        System.out.print(each2 + " ");
+                    }
+                    System.out.println();
+                }
+            }
+
 
         }
     }
