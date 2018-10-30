2ccafbe0b2bbabd8d47bc29fc7b44a85668b4a11
akhil3060 <Akhil3060@msitprogram.net>
2018-10-30 09:42:10 +0530

    "ads2_m1_a1_1: 6/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m1/1.1 Graph/Solution.java
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Collections;
 
 
 interface Graph {
@@ -21,66 +25,140 @@ class Solution {
         Scanner sc = new Scanner(System.in);
         String type = sc.nextLine();
         String[] nodes = new String[2];
-        int a, b;
+        int v, e1;
         if (type.equals("List")) {
-            HashMap<String, ArrayList<String>> hmap = new HashMap<>();
-            ArrayList<String> arrlist = new ArrayList<>();
-            int v = 0, e1 = 0;
-            // System.out.println("v =" + v);
+
             v = Integer.parseInt(sc.nextLine());
             e1 = Integer.parseInt(sc.nextLine());
+            String[] input = sc.nextLine().split(",");
 
-
-            int[][] graph = new int[v][v];
-
-            String[] line = sc.nextLine().split(",");
-
-            // System.out.println(line[1]);
-            // System.out.println("aaaaa" + graph.length);
-
-            while (sc.hasNext()) {
-                nodes = sc.nextLine().split(" ");
-                a = Integer.parseInt(nodes[0]);
-                b = Integer.parseInt(nodes[1]);
-
-                // try {
-                if (a != b) {
-                    graph[a][b] = 1;
-                    graph[b][a] = 1;
+            GraphList g = new GraphList(v);
+            for (int i = 0; i < e1; i++) {
+                String[] add = sc.nextLine().split(" ");
+                int a = Integer.parseInt(add[0]);
+                int b = Integer.parseInt(add[1]);
+                if (a != b && !g.hasEdge(a, b)) {
+                    g.addEdge(a, b);
                 }
-
-                // } catch (Exception e) {
-                // System.out.println("A " + a + "  b = " + b);
-                // }
-
             }
-            for (int[] each : graph) {
-                for (int each2 : each) {
-                    if (each2 == 1) {
-                        arrlist.add(nodes[each2]);
-                        // hmap.put()
-                        // arrlist.put(nodes[]);
-                    }
-                    hmap.put(nodes[each[0]], arrlist);
-                    // System.out.println("arrlist " + arrlist);
-                }
-                // System.out.println();
-            }
-            if (v == 1) {
-                System.out.println(v + " vertices, " + (e1 - 1) + " edges");
+            System.out.println(g.display(input));
 
-            } else {
-                System.out.println(v + " vertices, " + e1 + " edges");
 
-            }
-            if (v <= 1) {
-                System.out.println("No edges");
-            }
-            // System.out.println(hmap.get("Andhra Pradesh"));
+            // HashMap<String, ArrayList<String>> hmap = new HashMap<String, ArrayList<String>>();
+            // ArrayList<String> arrlist = new ArrayList<>();
+            // int v = 0, e1 = 0;
+            // // System.out.println("v =" + v);
+            // v = Integer.parseInt(sc.nextLine());
+            // e1 = Integer.parseInt(sc.nextLine());
 
+
+            // int[][] graph = new int[v][v];
+
+            // String[] line = sc.nextLine().split(",");
+
+            // // System.out.println(line[1]);
+            // // System.out.println("aaaaa" + graph.length);
+
+            // while (sc.hasNext()) {
+            //     nodes = sc.nextLine().split(" ");
+            //     a = Integer.parseInt(nodes[0]);
+            //     b = Integer.parseInt(nodes[1]);
+
+            //     // try {
+            //     if (a != b) {
+            //         graph[a][b] = 1;
+            //         graph[b][a] = 1;
+            //     }
+
+            //     // } catch (Exception e) {
+            //     // System.out.println("A " + a + "  b = " + b);
+            //     // }
+
+            // }
+            // // System.out.println(Arrays.deepToString(graph));
+            // // for (int[] each : graph) {
+            // //     for (int each2 : each) {
+            // //         System.out.print(each2 + " ");
+            // //     }
+            // //     System.out.println();
+            // // }
+            // // System.out.println(Arrays.toString(line));
+            // // for (int[] each : graph) {
+            // //     arrlist.clear();
+            // //     for (int each2 : each) {
+            // //         if (each2 == 1) {
+            // //             // System.out.println("each 2"+each2);
+            // //             arrlist.add(line[each2]);
+            // //             // hmap.put()
+            // //             // arrlist.put(line[]);
+            // //         }
+            // //         hmap.put(line[each[0]], arrlist);
+            // //     }
+            // //     System.out.println("arrlist " + arrlist);
+
+            // //     System.out.println();
+            // // }
+
+            // for (int i = 0; i < v; i++) {
+            //     arrlist = new ArrayList<String>();
+            //     for (int j = 0; j < v; j++) {
+            //         if (graph[i][j] == 1) {
+            //             // System.out.println("adding ="+line[j]);
+            //             arrlist.add(line[j]);
+            //         }
+            //     }
+            //     hmap.put(line[i], arrlist);
+            //     // System.out.println("========== line[i] ="+line[i]);
+            //     // System.out.println("aaray list ="+arrlist);
+            //     // System.out.println(line[i] + ":" + hmap.get(line[i]));
+            //     System.out.println(hmap+"\n");
+
+            // }
+            // // System.out.println("etst " + line[5] + ":" + hmap.get(line[5]));
+            // // System.out.println("etst " + line[6] + ":" + hmap.get(line[6]));
+            // // System.out.println("etst " + line[7] + ":" + hmap.get(line[7]));
+
+            // // System.out.println("________________________________-");
+            // if (v == 1) {
+            //     System.out.println(v + " vertices, " + (e1 - 1) + " edges");
+
+            // } else {
+            //     System.out.println(v + " vertices, " + e1 + " edges");
+
+            // }
+            // if (v <= 1) {
+            //     System.out.println("No edges");
+            // }
+            // // for (int i = 0; i < v; i++) {
+            // //     // System.out.println(line[i]);
+            // //     System.out.println(line[i] + ":" + hmap.get(line[i]));
+
+            // // }
+            // // Set s = hmap.keySet();
+            // // for (String name : hmap.keySet()) {
+
+            // //     String key = name.toString();
+            // //     String value = hmap.get(name).toString();
+            // //     System.out.println(key + " " + value);
+            // // }
+            // // System.out.println("Bihar  " + hmap.get("Bihar"));
+            // List<String> list = new ArrayList<String>(hmap.keySet());
+            // Set<String> keySet = hmap.keySet();
+            // // keySet = hmap.keySet();
+            // ArrayList<String> listOfKeys = new ArrayList<String>(keySet);
+            // // System.out.println(listOfKeys);
+            // Collections.sort(listOfKeys);
+
+            // // System.out.println(listOfKeys);
+            // // System.out.println(hmap);
+            // for(String name :listOfKeys ){
+            //     // System.out.println(name+":"+ hmap.get(name));
+            //     System.out.println(name+":"+ hmap.get(name).toString());
+            // }
 
         } else if (type.equals("Matrix")) {
-            int v = 0, e1 = 0;
+            v = 0;
+            e1 = 0;
             v = Integer.parseInt(sc.nextLine());
             e1 = Integer.parseInt(sc.nextLine());
             // System.out.println("e =" + e1);
@@ -89,7 +167,7 @@ class Solution {
             int[][] graph = new int[v][v];
 
             // System.out.println(Arrays.deepToString(graph));
-
+            int a, b;
             while (sc.hasNext()) {
                 nodes = sc.nextLine().split(" ");
                 a = Integer.parseInt(nodes[0]);
