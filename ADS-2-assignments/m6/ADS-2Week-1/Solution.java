5b7e31049a4280b041dbb7c6ee16de9008198191
akhil3060 <Akhil3060@msitprogram.net>
2018-11-05 09:49:28 +0530

    "ads2_m6_a1_1: 4/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignments/m6/ADS-2Week-1/Solution.java
@@ -1,200 +1,107 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-// import java.io.File;
-// import java.util.Arrays;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 
-/**
- * Class for page rank.
- */
 class PageRank {
-    /**
-     * Constructs the object.
-     */
-    PageRank() {
-
-    }
-    /**
-     * Puts a pr.
-     */
-    void putPR() {
-
-    }
-    // double getPR(int v) {
-
-    // }
-    // String toString() {
-
-    // }
-
+	HashMap<Integer, Double>map;
+	Digraph graph;
+	Digraph revGraph;
+	private int vertices;
+	PageRank(Digraph dg) {
+		graph=dg;
+		vertices = graph.V();
+		map = new HashMap<Integer, Double>();
+		revGraph = graph.reverse();
+	}
+	public void calculatePr() {
+		Double sum = 0.0;
+		int count = 0;
+		double temp = (double) vertices;
+		double initialPr = (1/temp);
+		for(int i = 0; i < vertices; i++) {
+			if(graph.indegree(i) == 0) {
+				//System.out.println("i am here superman");
+				map.put(i, 0.0);
+			} else {
+				map.put(i, initialPr);
+			}
+		}
+		double[] tempArray = new double[graph.V()];
+		for( int j = 0;j < 1000; j++) {
+			for( int i = 0; i < vertices; i++) {
+				//System.out.println("---------------------------------");
+				sum = 0.0000;
+				for(int each : revGraph.adj(i)) {
+					double value = map.get(each);
+					sum += ((double)value/(double)graph.outdegree(each));
+				}
+				tempArray[i] = sum;
+			}
+			for(int i = 0; i < vertices; i++) {
+				map.put(i, tempArray[i]);
+			}
+		}
+	}
+	public void print() {
+		for( int i = 0; i<map.size(); i++) {
+			System.out.println(i + " - " + map.get(i));
+		}
+	}
 }
-/**
- * Class for web search.
- */
+
 class WebSearch {
-    /**
-     * Constructs the object.
-     *
-     * @param      rk    { parameter_description }
-     * @param      name  The name
-     */
-    WebSearch(final PageRank rk, final String name) {
-
-    }
-
-}
-
-/**
- * Class for solution.
- */
-final class Solution {
-    /**
-     * Constructs the object.
-     */
-    private Solution() {
-
-    }
-    /**
-     * Main.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        // read the first line of the input to get the number of vertices
-
-        // iterate count of vertices times
-        // to read the adjacency list from std input
-        // and build the graph
-
-
-        // Create page rank object and pass the graph object to the constructor
-
-        // print the page rank object
-
-        // This part is only for the final test case
-
-        // File path to the web content
-        String file = "WebContent.txt";
-
-        // instantiate web search object
-        // and pass the page rank object and the file path to the constructor
-        // read the search queries from std in
-        // remove the q= prefix and extract the search word
-        // pass the word to iAmFeelingLucky method of web search
-        // print the return value of iAmFeelingLucky
-
-        HashMap<Integer, Integer> mpin = new HashMap<>();
-        HashMap<Integer, ArrayList<Integer>> mpinList = new HashMap<>();
-        HashMap<Integer, Integer> mpout = new HashMap<>();
-        HashMap<Integer, Double> curPR = new HashMap<>();
-        Scanner sc = new Scanner(System.in);
-        int v = Integer.parseInt(sc.nextLine());
-        int[][] graph = new int[v][v];
-        int a, b;
-        int c = v;
-        int count = 0;
-        int e = 0;
-        ArrayList<Integer> li = new ArrayList<>();
-        while (v > 0) {
-            li = new ArrayList<>();
-            v--;
-            a = 0;
-            b = 0;
-            String[] line = sc.nextLine().split(" ");
-            a = Integer.parseInt(line[0]);
-            mpout.put(a, line.length - 1);
-            final double inValue = 0.25;
-            curPR.put(a, inValue);
-            for (int i = 1; i < line.length; i++) {
-                b = Integer.parseInt(line[i]);
-                if (a != b) {
-                    graph[a][b] = 1;
-                }
-                e++;
-                li = mpinList.get(b);
-                // try {
-                //     li = mpinList.get(b);
-                // } catch (Exception g) {
-                //     // li = new ArrayList<>();
-                //     li.add(a);
-                //     mpinList.put(b, li);
-                // }
-                if (li == null) {
-                    ArrayList<Integer> li1 = new ArrayList<>();
-                    li1.add(a);
-                    mpinList.put(b, li1);
-                } else {
-                    li.add(a);
-                    mpinList.put(b, li);
-                }
-                // li = mpinList.get(b);
-
-
-
-
-
-                try {
-                    count = mpin.get(b);
-
-                } catch (Exception f) {
-                    mpin.put(b, 1);
-                    // li.add(b);
-                    // mpinList.put(b, li);
-                    continue;
-                }
-                // li.add(b);
-                // mpinList.put(b, li);
-                mpin.put(b, count + 1);
-            }
-        }
-
-        // System.out.println(mpin);
-        // System.out.println(mpout);
-        // System.out.println(mpinList);
-        // System.out.println(curPR);
-        System.out.println(c + " vertices, " + e + " edges ");
-        // System.out.println(Arrays.deepToString(graph));
-        for (int i = 0; i < c; i++) {
-            // System.out.println("test 1");
-            System.out.print(i + ": ");
-            for (int j = c - 1; j >= 0; j--) {
-                // System.out.println("test 2");
-                if (graph[i][j] == 1) {
-                    System.out.print(j + " ");
-                }
-            }
-            System.out.println();
-        }
-
-        Double val = 0.0;
-        final int size = 1000;
-        for (int j = 0; j < size; j++) {
-            for (int i = 0; i < c; i++) {
-                val = 0.0;
-                try {
-                    for (int each : mpinList.get(i)) {
-                        val  += (curPR.get(each)) / (mpout.get(each));
-                    }
-                    curPR.put(i, val);
-                } catch (Exception h) {
-                    // System.out.println(i);
-                    curPR.put(i, 0.0);
-                }
-
-                // System.out.println("Iter =" + j + "-" + curPR);
-            }
-            // System.out.println();
-        }
-        System.out.println();
-
-        // System.out.println(curPR);
-
-        for (int k : curPR.keySet()) {
-            System.out.println(k + " - " + curPR.get(k));
-        }
-    }
 
 }
 
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int totalVertices = Integer.parseInt(sc.nextLine());
+		//sc.nextLine();
+		Digraph d = new Digraph(totalVertices);
+		int i = 0;
+		while (i < totalVertices) {
+			String[] arra1 = sc.nextLine().split(" ");
+			for (int j = 1; j < arra1.length; j++) {
+				d.addEdge(Integer.parseInt(arra1[0]), Integer.parseInt(arra1[j]));
+			}
+			i++;
+		}
+		System.out.println(d.toString());
+		for (int k = 0; k < d.V(); k++) {
+			//System.out.println(d.adj(k));
+			if (d.outdegree(k) == 0) {
+				//System.out.println("i am here");
+				for (int a = 0; a < d.V(); a++ ) {
+					if (k != a) {
+						d.addEdge(k, a);
+					}
+				}
+			}
+		}
+		PageRank pr = new PageRank(d);
+		pr.calculatePr();
+		pr.print();
+		//System.out.println(d.toString());
+		//System.out.println(d.reverse());
+		// Create page rank object and pass the graph object to the constructor
+
+		// print the page rank object
+
+		// This part is only for the final test case
+
+		// File path to the web content
+		String file = "WebContent.txt";
+
+		// instantiate web search object
+		// and pass the page rank object and the file path to the constructor
+
+		// read the search queries from std in
+		// remove the q= prefix and extract the search word
+		// pass the word to iAmFeelingLucky method of web search
+		// print the return value of iAmFeelingLucky
+
+	}
+}
