057147ad02f7747d1961644fdcb23340afb3e116
akhil3060 <Akhil3060@msitprogram.net>
2018-11-19 02:03:32 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m14/Assignment-1/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		String[] wordList = loadwordLists();
+        //Your code goes here...
+        TST tst = new TST();
+        for (int i = 0; i < wordList.length; i++) {
+            int n = wordList[i].length();
+            String[] suffixes = new String[n];
+            for (int j = 0; j < n; j++) {
+                suffixes[j] = wordList[i].substring(j, n);
+                tst.put(suffixes[j], 0);
+
+            }
+
+        }
+        Scanner sc = new Scanner(System.in);
+        Iterable<String> st = tst.keysWithPrefix(sc.nextLine());
+
+        for (String s : st) {
+            System.out.println(s.toString());
+        }
+	}
+
+	public static String[] loadwordLists() {
+		In in = new In("/Files/dictionary-algs4.txt");
+		String[] wordLists = in.readAllStrings();
+		return wordLists;
+	}
+}
