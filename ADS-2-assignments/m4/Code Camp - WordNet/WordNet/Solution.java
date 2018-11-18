a66901325246b2ef77480077aadc68c9b849a74b
akhil3060 <Akhil3060@msitprogram.net>
2018-11-19 01:02:27 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -5,37 +5,43 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		try {
-			WordNet wordNet = new WordNet("Files/" + sc.nextLine(), "Files/" + sc.nextLine());
-			switch (sc.nextLine()) {
-			case "Graph": System.out.println(wordNet.g);
-				break;
-			case "Queries":
-				while (sc.hasNextLine()) {
-					String[] tokens = sc.nextLine().split(" ");
-					Integer dist = wordNet.distance(tokens[0], tokens[1]);
-					String ancestor = wordNet.sap(tokens[0], tokens[1]);
-					System.out.println("distance = " + dist + ", " + "ancestor = " + ancestor);
-				}
-			}
-		} catch (NullPointerException f) {
-			System.out.println("IllegalArgumentException");
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            WordNet wordNet = new WordNet("Files/" +
+                                          sc.nextLine(), "Files/" +
+                                          sc.nextLine());
+            switch (sc.nextLine()) {
+            case "Graph": System.out.println(wordNet.g);
+                break;
+            case "Queries":
+                while (sc.hasNextLine()) {
+                    String[] tokens = sc.nextLine().split(" ");
+                    Integer dist = wordNet.distance(tokens[0], tokens[1]);
+                    String ancestor = wordNet.sap(tokens[0], tokens[1]);
+                    System.out.println("distance = " + dist +
+                                       ", " + "ancestor = " + ancestor);
 
-	}
+                }
+            default:
+
+            }
+        } catch (NullPointerException f) {
+            System.out.println("IllegalArgumentException");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
