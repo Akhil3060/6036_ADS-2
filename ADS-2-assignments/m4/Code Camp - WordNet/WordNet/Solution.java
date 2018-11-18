a4ac42cd8bcdd998c032c907e274d1633bf7f724
akhil3060 <Akhil3060@msitprogram.net>
2018-11-19 00:55:46 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,25 +1,34 @@
-import java.util.*;
-class Solution {
+import java.util.Scanner;
+
+
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		try {
-			WordNet wordNet = new WordNet("Files/"+sc.nextLine(), "Files/"+sc.nextLine());
+			WordNet wordNet = new WordNet("Files/" + sc.nextLine(), "Files/" + sc.nextLine());
 			switch (sc.nextLine()) {
-				case "Graph": System.out.println(wordNet.g);
-							  break;
-				case "Queries":
-					while(sc.hasNextLine()){
-						String[] tokens = sc.nextLine().split(" ");
-						Integer dist = wordNet.distance(tokens[0], tokens[1]);
-						String ancestor = wordNet.sap(tokens[0], tokens[1]);
-						System.out.println("distance = " + dist + ", " + "ancestor = " + ancestor);
-					}
+			case "Graph": System.out.println(wordNet.g);
+				break;
+			case "Queries":
+				while (sc.hasNextLine()) {
+					String[] tokens = sc.nextLine().split(" ");
+					Integer dist = wordNet.distance(tokens[0], tokens[1]);
+					String ancestor = wordNet.sap(tokens[0], tokens[1]);
+					System.out.println("distance = " + dist + ", " + "ancestor = " + ancestor);
+				}
 			}
-		}
-		catch (NullPointerException f) {
+		} catch (NullPointerException f) {
 			System.out.println("IllegalArgumentException");
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
